4e3011542e6c8a76f51d2a3013258fcb29379bb5
Nathan Fisk <triphazard@gmail.com>
2023-02-13 20:26:21 +0000

    Subtests and Helper Function in Tests

--- 01.hello_world/hello_test.go
+++ 01.hello_world/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Fisk")
-	want := "Hello, Fisk"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Fisk")
+		want := "Hello, Fisk"
+		assertCorrectMessage(t,got,want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t,got,want)
+	})
+}
 
+// Helper function to reduce repeated code in subtests above
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper we point error at parent test
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q but want %q", got, want)
+		t.Errorf("got %q, but want %q", got, want)
 	}
 }
