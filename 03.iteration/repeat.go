6ebd3b3ebc0564656422e9dc791039bfc10dcec5
Nathan Fisk <triphazard@gmail.com>
2023-02-16 08:25:59 +0000

    Iteration

---
+++ 03.iteration/repeat.go
@@ -0,0 +1,9 @@
+package iteration
+
+func Repeat(character string, iterations int) string {
+	var repeated string
+	for i := 0; i < iterations; i++ {
+		repeated += character
+	}
+	return repeated
+}
